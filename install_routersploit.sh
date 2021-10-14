05897e257a468eca7e346adea1d2533f0ab0eab5
MEGABOT-HACK <92226109+MEGABOT-HACK@users.noreply.github.com>
2021-10-14 16:17:34 -0500

    Update install_routersploit.sh

--- install_routersploit.sh
+++ install_routersploit.sh
@@ -3,11 +3,13 @@ apt install python
 apt install python2
 git clone https://www.github.com/threat9/routersploit
 cd routersploit
-python3 -m pip install -r requirements.txt
-pip install future
-pip install requests
-pip install paramiko
-pip install pysnmp
-pip install pycrypto
+python3 -m pip2 install -r requirements.txt
+pip2 install future
+pip2 install requests
+pip2 install paramiko
+pip2 install pysnmp
+pip2 install pycrypto
 chmod +x *
+clear
+rm install_routersploit.sh
 python3 rsf.py
